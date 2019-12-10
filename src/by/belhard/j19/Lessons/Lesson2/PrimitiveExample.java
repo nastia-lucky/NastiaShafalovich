cbf3c04e4deb613728bce338981b99d7e0835ae2
Luba <lubakozus@gmail.com>
2019-12-10 20:36:36 +0300

    init

---
+++ src/by/belhard/j19/Lessons/Lesson2/PrimitiveExample.java
@@ -0,0 +1,19 @@
+package by.belhard.j19.Lessons.Lesson2;
+
+public class PrimitiveExample {
+    public static void main(String[] args) {
+        /*hgfkhjkgfjhkgkhjk
+        hgfjhkfghkfg
+         */
+
+        /*short sh1=5;
+        int i=100;
+        double d=3.14;
+
+        System.out.println(sh1 + i/ d);
+        */
+        int i=0;
+        System.out.println(i++);
+        System.out.println(++i);
+    }
+}
